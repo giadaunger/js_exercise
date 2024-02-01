c01a6aefdaa8728b71e918ef53dfb90fb7ec1400
Giada Unger <giada.unger02@gmail.com>
2024-02-01 10:45:12 +0100

    first commit

---
+++ cart.js
@@ -0,0 +1,19 @@
+// Export nr 1
+export function calculate(){
+    console.log("Using calculate")
+}
+
+export default function default_calculate(){
+    console.log("Using default")
+}
+
+// Export nr 2
+// function calculate(){
+//     console.log("Using calculate")
+// }
+
+// function calculate_hypothenuse(height, length){
+// Math.sqrt(height**2 + length**2)
+// }
+
+// export { calculate, calculate_hypothenuse }
